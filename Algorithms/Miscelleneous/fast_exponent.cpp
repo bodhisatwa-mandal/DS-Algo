23e5afcd49f30dc4959853d917f029616569d0c2
bodhisatwa-mandal <bodhisatwam@gmail.com>
2017-11-26 20:13:03 +0530

    Fast Exponent

---
+++ Algorithms/Miscelleneous/fast_exponent.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+long long int fast_exp(int base, int exp)
+{
+    if(exp==1)
+    return base;
+    else
+    {
+        if(exp%2 == 0)
+        {
+            long long int base1 = pow(fast_exp(base, exp/2),2);
+            if(base1 >= 1000000007)
+            return base1%1000000007;
+            else
+            return base1;
+        }
+        else
+        {
+            long long int ans = (base*  pow(fast_exp(base,(exp-1)/2),2));
+            if(ans >= 1000000007)
+            return ans%1000000007;
+            else
+            return ans;
+        }
+    }
+}
+int main()
+{
+	long long int base,exp;
+	cin>>base>>exp;
+	cout<<fast_exp(base,exp)<<endl;
+	return 0;
+}
